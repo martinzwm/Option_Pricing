b636ac83e7f66a4eeea225dcb4e644e74b1a37b6
Martin Ma <z74ma@uwaterloo.ca>
2022-04-25 23:55:14 -0400

    Add example for generating trajectories

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from transition import BrownianMotion
 from LSM import AmericanOptionsLSMC
+import matplotlib.pyplot as plt
 
-AmericanPUT = AmericanOptionsLSMC('put', 36., 40., 1., 50, 0.06, 0, 0.2, 10000, BrownianMotion)
+# Test baseline - American Put LSM method
+AmericanPUT = AmericanOptionsLSMC(option_type='put', S0=36, strike=40, T=1, M=50,
+                                  r=0.06, div=0, sigma=0.2, N=10000, transition=BrownianMotion)
 print('Price: ', AmericanPUT.price)
+
+# Generate trajectories
+traj_gen = BrownianMotion(S0=36, r=0.06, sigma=0.2, T=1, M=50, N=10000)
+traj1 = traj_gen.simulate()[:,10] # take a look at the 10th trajectory
+# plt.plot(traj1)
+# plt.savefig("test.png")
