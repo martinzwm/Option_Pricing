5d341ac84a2c471449b650a006fd82529547378c
Anzoteh96 <anzoteh@hotmail.com>
2022-05-04 22:48:01 -0400

    added actor critic for PG

--- PG.py
+++ PG.py
@@ -1,16 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch.autograd import Variable
 from torch.distributions import Categorical
-import matplotlib.pyplot as plt
-from transition import BrownianMotion
 from torch.utils.data import Dataset, DataLoader
 from torch.optim import *
 from utility import SimDataset
-from tqdm import tqdm
+from transition import BrownianMotion
+from LSM import AmericanOptionsLSMC
 
 # For now just copy paste the code from the Colab solution (PSet 6)
 class PGNetwork(nn.Module):
@@ -37,8 +37,7 @@ class PGNetwork(nn.Module):
     def forward(self, observations):
         B, N = observations.size()
         # N should really be 2, one for time and . 
-        try:
-            x = self.net(observations)
+        x = self.net(observations)
         return Categorical(logits = F.log_softmax(x, dim = 1))
 
 class ValueNetwork(nn.Module):
@@ -51,7 +50,6 @@ class ValueNetwork(nn.Module):
             nn.ReLU(),
             nn.Linear(hidden_dim, 1)
         )
-        # TODO: add the Actor-Critic version (pass in the feature vector). 
 
     def forward(self, observation):
         B, L = observation.size()
@@ -59,12 +57,40 @@ class ValueNetwork(nn.Module):
 
 # Next, compute the discounted returns, same as how we did in PSet 6. 
 def discounted_returns(rewards, gamma, device = None):
+    """
+        Args:
+            rewards: B x L, B = # trajectory, L = length of each trajectory. 
+            gamma: float
+    """
     returns = torch.zeros_like(rewards, device = device)
     returns[:, -1] = rewards[:, -1]
     for i in range(rewards.shape[1] - 1, 0, -1):
         r = rewards[:, i - 1]
         returns[:, i-1] = r + gamma * returns[:, i]
     return returns
+
+def get_Q(price, payoff, gamma):
+    """
+        Args:
+            price: B x L
+            payoff: B x L, B = num_batches
+        Returns:
+            Q: B x L
+    """
+    B, L = payoff.shape
+    q_matrix = np.zeros_like(payoff)
+    q_matrix[:, -1] = payoff[:, -1]
+    for t in range(L - 2, -1, -1):
+        # In the case where price[:, t] are all the same, we just take the mean. 
+        # This would happen in the beginning of the trajectory. 
+        if np.var(price[:, t]) < 1e-9:
+            cont_val = np.mean(q_matrix[:, t + 1]) * gamma
+        else:
+            regress = np.polyfit(price[:, t], q_matrix[:, t + 1] * gamma, 5)
+            cont_val = np.polyval(regress, price[:, t])
+        q_matrix[:, t] = np.where(payoff[:, t] > cont_val, payoff[:, t], 
+                                  q_matrix[:, t + 1] * gamma)
+    return q_matrix
 
 def update_parameters(optimizer, model, rewards, log_probs, gamma,
                       values = None, temporal = False, device = None):
@@ -103,7 +129,7 @@ def reward_func(traj, strike, style = None):
     # TODO: change this into some sort of TD(0) algo. 
     return reward
 
-def rollout(model, traj, strike, dt, vmodel=None, device=None):
+def rollout(model, traj, strike, dt, gamma, vmodel=None, device=None):
     """
         Args:
             model: the PG model (presumably, no other choice)
@@ -118,6 +144,9 @@ def rollout(model, traj, strike, dt, vmodel=None, device=None):
     exercised = torch.zeros((B), device = device)
     reward_style = None
     reward_list = reward_func(traj[:, :, 0], strike, reward_style)
+    q_all = get_Q(traj[:, :, 0].numpy(), reward_list.numpy(), gamma)
+    q_all = torch.from_numpy(q_all).float()
+    q_state = q_all[:, 0]
     if reward_style == "TD0":
         base_val = max(strike - traj[0, 0, 0].item(), 0)
         ep_reward.fill_(base_val)
@@ -131,8 +160,11 @@ def rollout(model, traj, strike, dt, vmodel=None, device=None):
         action = torch.logical_and(action, torch.logical_not(exercised))
         exercised = torch.logical_or(action, exercised)
         if vmodel: # We'll fix this later.
-            value = vmodel(state)
-            values[:, T] = value
+            #value = vmodel(state)
+            #values[:, T] = value
+            if T < L - 1:
+                q_state = torch.where(exercised, q_state, q_all[:, T + 1])
+            values[:, T] = q_state
         
         if reward_style is None:
             if T < L - 1:
@@ -151,6 +183,7 @@ def rollout(model, traj, strike, dt, vmodel=None, device=None):
         log_probs[:, T] = log_prob
     #print(actions)
     #print(rewards)
+    # from IPython import embed; embed()
     return actions, rewards, log_probs, values, ep_reward
 
 def data_load_pg(traj, train_test_split=0.8):
@@ -172,20 +205,33 @@ def data_load_pg(traj, train_test_split=0.8):
 
     return train_set, test_set
 
-def pg_train():
+def pg_train(TEMPORAL = False, USE_VALUE_NETWORK = False, fname = None):
+    strike, S0, r, sigma, T, M, N, transition = 40, 36, 0.06, 0.2, 1, 50, 1000, BrownianMotion
+    div = 0
+    transition_model = BrownianMotion(S0, r, sigma, T, M, N)
+    data = np.transpose(transition_model.simulate())
+    lsm = AmericanOptionsLSMC('put', S0, strike, T, M, r, div, sigma, N, transition)
+    torch.manual_seed(123)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if device == torch.device('cuda'):
         batch_size = 20
     else:
         batch_size = 20
-    strike, S0, r, sigma, T, M, N, transition = 40, 36, 0.06, 0.2, 1, 50, 1000, BrownianMotion
-    transition_model = BrownianMotion(S0, r, sigma, T, M, N)
-    data = np.transpose(transition_model.simulate())
+    LR = 5e-4
+    USE_VALUE_NETWORK = USE_VALUE_NETWORK
+    TEMPORAL = TEMPORAL
+    model = PGNetwork(in_dim = 2, out_dim = 2)
+    dt = T / M
+    gamma = np.exp(-r * dt)
+    if USE_VALUE_NETWORK:
+        vmodel = ValueNetwork(2, 64).to(device)
+        optimizer = optim.Adam(list(model.parameters()) + list(vmodel.parameters()), lr=LR)
+    else:
+        optimizer = optim.Adam(model.parameters(), lr=LR)
     # TODO: data_load
     train_set, test_set = data_load_pg(data, 0.80)
     train_set = SimDataset(train_set)
     test_set = SimDataset(test_set)
-    dt = T / M
 
     train_loader = DataLoader(
         train_set,
@@ -199,22 +245,13 @@ def pg_train():
         num_workers = 1,
         shuffle = True
     )
-    LR = 1e-3
-    USE_VALUE_NETWORK = False
-    TEMPORAL = False
-    model = PGNetwork(in_dim = 2, out_dim = 2)
-    gamma = np.exp(-r * dt)
-    if USE_VALUE_NETWORK:
-        vmodel = ValueNetwork(2, 64).to(device)
-        optimizer = optim.Adam(list(model.parameters()) + list(vmodel.parameters()), lr=LR)
-    else:
-        optimizer = optim.Adam(model.parameters(), lr=LR)
-    num_epochs = 100 # Change later. 
-    for step in tqdm(range(num_epochs)):
+    num_epochs = 500 # Change later. 
+    for step in range(num_epochs):
         model.train()
-        for item in train_loader:
+        for item in tqdm(train_loader):
             actions, rewards, log_probs, values, ep_reward = rollout(
-                model, item, strike, dt, vmodel=vmodel if USE_VALUE_NETWORK else None, device=device)
+                model, item, strike, dt, gamma, 
+                vmodel=vmodel if USE_VALUE_NETWORK else None, device=device)
             # print(torch.mean(ep_reward))
 
             update_parameters(optimizer, model, rewards, log_probs, gamma, 
@@ -224,15 +261,19 @@ def pg_train():
         test_reward = []
         model.eval()
         if (step + 1) % 10 == 0:
-            for item in test_loader:
+            for item in tqdm(test_loader):
                 actions, rewards, log_probs, values, ep_reward = rollout(
-                    model, item, strike, dt, vmodel=vmodel if USE_VALUE_NETWORK else None, device=device)
+                    model, item, strike, dt, gamma, 
+                    vmodel=vmodel if USE_VALUE_NETWORK else None, device=device)
                 for rew in ep_reward:
                     test_reward.append(rew.item())
             print(sum(test_reward)/len(test_reward))
-            prices = np.linspace(np.min(data), max(strike, S0), 40)
+            prices = np.linspace(np.min(data), np.max(data), 40)
             dtimes = np.linspace(0, 1, 6)
-            prob_plot(model, prices, dtimes = dtimes, fname = "pg_plot.png") 
+            if fname is None:
+                fname = "pg_plot.png"
+            prob_plot(model, prices, dtimes = dtimes, fname = fname) 
+        print("Epoch {} done".format(step + 1))
         
 
 def prob_plot(model, prices, dtimes, fname = None):
@@ -263,4 +304,6 @@ def prob_plot(model, prices, dtimes, fname = None):
     plt.clf()
 
 if __name__ == "__main__":
-    pg_train()
+    pg_train(TEMPORAL = False, USE_VALUE_NETWORK = False, fname = "pg_vanilla.png")
+    pg_train(TEMPORAL = True, USE_VALUE_NETWORK = False, fname = "pg_temp.png")
+    pg_train(TEMPORAL = True, USE_VALUE_NETWORK = True, fname = "pg_critic.png")
